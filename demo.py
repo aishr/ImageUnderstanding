106dfe734057e2fee6ac4e340ec6eba70f32934e
Aishwarya Ramanathan <ARamanathan@indigo.ca>
2017-05-04 14:45:25 -0400

    got the range of colours working

--- demo.py
+++ demo.py
@@ -1,20 +1,39 @@
 import cv2
+import numpy as np
+from collections import Counter
+from colour import hex2web
 
-image = cv2.imread("wallet.jpg")
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-num = str(int(gray_image[0][0]))
-color_list = {}
-new_image = gray_image[:]
-for i in range(20):
-    for j in range(len(gray_image[i])):
-        if abs(int(num) - int(gray_image[i][j])) >= 30:
-            print(str(i) + " " + str(j))
-            color_list[[i,j]] = (image[i][i])
-        else:
-            new_image[i][j] = 0
-#cv2.imshow("Over the Clouds", image)
-#cv2.imshow("Over the Clouds - gray", gray_image)
-x = cv2.Canny(image,50, 150)
-cv2.imshow("edges", new_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def detectColour(imageName, backgroundColourSum, numOfColours):
+    image = cv2.imread(imageName)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    colour_list = []
+
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            num_sum = sum(image[i][j])
+            if num_sum <= backgroundColourSum - 60:
+                colour_list.append(image[i][j])
+    hex_values = []
+    for i in range(len(colour_list)):
+        hex_num = ''
+        for j in range(len(colour_list[i])):
+            if j > 0:
+                hex_num = hex_num + hex(colour_list[i][j])[2:]
+            else:
+                hex_num = hex_num + hex(colour_list[i][j])
+        hex_values.append(hex_num)
+        
+    blank_image = np.zeros((100,(numOfColours*50),3), np.uint8)
+    x = Counter(hex_values).most_common(numOfColours)
+    for i in range(numOfColours):
+        colour = [int(x[i][0][2:4],16), int(x[i][0][4:6],16), int(x[i][0][6:8],16)]
+        blank_image[:,(i*50):((i+1)*50)-1] = tuple(colour)
+
+    cv2.imshow("image", image)
+    cv2.imshow("test", blank_image) 
+    
+
+detectColour("box.jpg", 765, 20)
+        
+        
