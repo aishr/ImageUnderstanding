e6de250d8e3c81a011cc2eccfdc9d39bf349d54b
Aishwarya Ramanathan <ARamanathan@indigo.ca>
2017-05-09 11:39:21 -0400

    multicolour seems to be working

--- optimized.py
+++ optimized.py
@@ -31,29 +31,50 @@ def webSafeColour(num):
 
 def detectColour(imageName):
     colourData = makeDictionary()
-    image = imread(imageName)
-    edges = Canny(image, 50, 150, 3)
+    isoImage = imread(imageName)
+    origImage = imread(imageName)
+    edges = Canny(origImage, 50, 150, 3)
     colourList = []
-    backgroundColourSum = sum(image[0][0])
+    allColours = []
+    finalSelect = []
+    backgroundColourSum = sum(origImage[0][0])
 
-    for i in range(len(image)):
-        for j in range(len(image[i])):
-            numSum = sum(image[i][j])
+    for i in range(len(origImage)):
+        for j in range(len(origImage[i])):
+            numSum = sum(origImage[i][j])
             if numSum <= abs(backgroundColourSum - 60):
-                hexValue = RGB2Hex(image[i][j])                
+                hexValue = RGB2Hex(origImage[i][j])                
                 webSafe = webSafeColour(hexValue[4:]) + webSafeColour(hexValue[2:4]) + webSafeColour(hexValue[:2])
                 colourList.append(colourData[webSafe])
             else:
-                image[i][j] = [0,0,0]
+                isoImage[i][j] = [0,0,0]
     colFreq = Counter(colourList)
     totalPixels = sum(list(colFreq.values()))
-    colFreq = Counter(colourList).most_common(len(Counter(colourList)))
+    colFreq = colFreq.most_common(len(colFreq))
+    print("All Colours Detected:")
     for colour in colFreq:
-        print(colour[0] + ": " + str(round(colour[1]/totalPixels*100,0)) +"%")
-    imshow("image", image)
+        percent = round(colour[1]/totalPixels*100,0)
+        if str(percent) != '0.0':
+            print(colour[0] + ": " + str(percent) + "%")
+            allColours.append([colour[0],percent])
+    prevFreq = allColours[0][1]
+    finalSelect.append(allColours[0][0])
+    for i in range(1,3):
+        if prevFreq - allColours[i][1] <= 17.0:
+            finalSelect.append(allColours[i][0])
+        else:
+            break;
+        prevFreq = allColours[i][1]
+
+    print("\n")
+    print('Final Set of Colours (Total Count: ' + str(len(finalSelect)) + ')')
+    for i in range(len(finalSelect)):
+        print(str(i+1) + ") " + finalSelect[i])
+    imshow("image", origImage)
     imshow("edges", edges)
+    imshow("background", isoImage)
+
+    return finalSelect
     
-
-
 if __name__ == '__main__':
-    detectColour("images/infuser1.jpg")
+    detectColour("images/bottle6.jpg")
