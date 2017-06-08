4757f85027934c92632864e721c109348864052b
Aishwarya Ramanathan <ARamanathan@indigo.ca>
2017-06-08 14:36:25 -0400

    changed the script slightly

--- ColourDetector/testScripts/scriptfinal.py
+++ ColourDetector/testScripts/scriptfinal.py
@@ -1,8 +1,8 @@
 from optimized import detectColour
 import sys
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: scriptfinal dictionaryFile imageFile\n")
+        exit(1)
 
-if len(sys.argv) != 3:
-    print("Usage: scriptfinal dictionaryFile imageFile\n")
-    exit(1)
-
-detectColour(sys.argv[1], sys.argv[2])
+    detectColour(sys.argv[1], sys.argv[2])
